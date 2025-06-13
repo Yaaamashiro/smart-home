cc1e2ab08e6039ac368456867bdf331e246c3f61
Yaaamashiro <yaaamashiro@gmail.com>
2025-06-13 15:12:25 +0900

    change: rename Django app from smart_home to home

--- home/home/urls.py
+++ home/home/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for smart_home project.
+URL configuration for home project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
