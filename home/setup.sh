e736f0254981e88c2f30271e9a0cc7f72b0ddcd5
Yaaamashiro <yaaamashiro@gmail.com>
2025-06-26 14:16:25 +0900

    add: add Docker setup in home/setup.sh

--- home/setup.sh
+++ home/setup.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 set -e
 
-sudo apt-get update
-sudo apt-get upgrade -y
-
-sudo apt-get install -y libusb-1.0-0-dev make unzip wget
-
 cd $HOME
 
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y build-essential ca-certificates libusb-1.0-0-dev make unzip wget
+
+# install Docker
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker $USER
+
+#install bto_advanced_USBIR_cmd
 wget -N https://bit-trade-one.co.jp/wp/wp-content/uploads/mydownloads/bto_advanced_USBIR_cmd101.zip
 rm -rf bto_advanced_USBIR_cmd-1.0.1
 unzip bto_advanced_USBIR_cmd101.zip
 cd bto_advanced_USBIR_cmd-1.0.1
 
-# bto_advanced_USBIR_cmd.c の libusb_set_debug を libusb_set_option に置換
 sed -i 's/libusb_set_debug(ctx, 3);/libusb_set_option(ctx, LIBUSB_OPTION_LOG_LEVEL, 3);/' bto_advanced_USBIR_cmd.c
 
 sudo make
 sudo make install
-
-cd $HOME
 
 echo "Setup Successful!"
